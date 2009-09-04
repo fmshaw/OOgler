8aef623f337ce301636a0fabaf1bc9a720c168a3
Michael Shaw <genericness@gmail.com>
2009-09-04 15:04:47 +1000

    Add testing... this should have happened first!

--- OOgler.py
+++ OOgler.py
@@ -1,3 +1,12 @@
+"""
+This is the main module for launching the OOgler
+
+Here we are just testing that the scene root can be instansiated.
+
+>>> root = scene.root.SceneRoot()
+>>> root #doctest:+ELLIPSIS
+<scene.root.SceneRoot object at 0x...>
+"""
 import scene.root
 
 root = scene.root.SceneRoot()
